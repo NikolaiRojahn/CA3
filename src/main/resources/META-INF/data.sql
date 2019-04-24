a44dde74b3606c214c8af7430d9bfec540a497a5
NikolaiRojahn <cph-nr87@cphbusiness.dk>
2019-04-24 15:26:07 +0200

    Trying to solve internal server error during login

--- src/main/resources/META-INF/data.sql
+++ src/main/resources/META-INF/data.sql
@@ -3,5 +3,5 @@ INSERT INTO users (user_name, user_pass) VALUES ('Morten', '$2a$10$AVa8EtghlpgHD
 INSERT INTO users (user_name, user_pass) VALUES ('Nikolai', '$2a$10$ite4/ewE/U1Ehd55el06N.Ccbm4ZglNF9zvt5Z6fGTTmyh3p8Kcdi');
 INSERT INTO roles (role_name) VALUES ('user');
 INSERT INTO roles (role_name) VALUES ('admin');
-INSERT INTO user_roles (user_name, role_name) VALUES ('user', 'user');
-INSERT INTO user_roles (user_name, role_name) VALUES ('admin', 'admin');
+INSERT INTO user_roles (user_name, role_name) VALUES ('Morten', 'user');
+INSERT INTO user_roles (user_name, role_name) VALUES ('Nikolai', 'admin');

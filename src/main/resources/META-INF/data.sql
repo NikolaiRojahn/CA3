961d48b2022e2ced86733e4e99ad73c4c2d7aa99
NikolaiRojahn <cph-nr87@cphbusiness.dk>
2019-04-24 13:22:14 +0200

    Almost done with start-up project

---
+++ src/main/resources/META-INF/data.sql
@@ -0,0 +1,7 @@
+USE ca3;
+INSERT INTO users (user_name, user_pass) VALUES ('user', 'test');
+INSERT INTO users (user_name, user_pass) VALUES ('admin', 'test');
+INSERT INTO roles (role_name) VALUES ('user');
+INSERT INTO roles (role_name) VALUES ('admin');
+INSERT INTO user_roles (user_name, role_name) VALUES ('user', 'user');
+INSERT INTO user_roles (user_name, role_name) VALUES ('admin', 'admin');
